9e5f287c824118ed55cf02cbc2ff995d0e5e805a
johnchiong <152459351+johnchiong@users.noreply.github.com>
2025-02-18 15:59:53 +0800

    PHP_practice3 and Change the part1_questions to pdf

---
+++ PHP_practice3.php
@@ -0,0 +1,21 @@
+<?php
+$matrix = [
+    [12, 23, 34],
+    [45, 55, 62],
+    [71, 84, 90]
+];
+
+$row = 0;
+
+while ($row < count($matrix)) {
+    $col = 0;
+
+    while ($col < count($matrix[$row])) {
+        if ($matrix[$row][$col] % 2 == 0) {
+            echo $matrix[$row][$col]. " ";
+        }
+        $col++;
+    }
+    $row++;
+}
+?>
